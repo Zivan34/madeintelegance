89a28b560f3f7af0b5c8889c0f3ee57e542e0213
Kerem Cem Torun <keremcemtorun@gmail.com>
2024-03-04 21:57:20 +0100

    clear pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [message, setMessage] = useState("")
 
   return (
     <>
-      <h1>Ben Kerem, Selamlar :)</h1>
-      <div className="card">
-        <button onClick={() => setMessage("Seni Seviyorum <3")}>
-          Yıldız buraya dokunurmusun?
-        </button>
-        <h3>
-          {message}
-        </h3>
-      </div>
+      
     </>
   )
 }
