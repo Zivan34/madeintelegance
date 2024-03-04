6b26a7a8e1c1af5494d48dc4830f06c1d2095294
Kerem Cem Torun <keremcemtorun@gmail.com>
2024-03-05 00:02:09 +0100

    created placeholder page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-
+import logo from "./assets/images/logo.png"
 function App() {
   const [message, setMessage] = useState("")
 
   return (
-    <>
-      
-    </>
+    <div align="center">
+      <img style={{ "height": "50rem" }} src={logo}></img>
+      <div style={{"marginTop":"-100px"}}>
+        <h1 className="mie-textcolorwhite">MadeIntElegance</h1>
+        <h3 className="mie-textcolorwhite">made with intelligence for elegance</h3>
+      </div>
+    </div>
   )
 }
 
